6595e34f1f4817970629e7d2f404d5e8e635dc63
Gaalpos <gaalpos@proton.me>
2025-09-22 02:02:20 +0200

    new things

--- index.js
+++ index.js
@@ -8,21 +8,79 @@ const plus1Guest = document.getElementById("add-one-guest")
 const plus2Guest = document.getElementById("add-two-guest")
 const plus3Guest = document.getElementById("add-three-guest")
 
+let homeBorder = document.getElementById('home-counter')
+let guestBorder = document.getElementById('guest-counter')
+
+
+let clearButton = document.getElementById("clear-btn")
+let saveButton = document.getElementById("save-btn")
+
+let results = document.getElementById("matches")
 
 
 let homeCounter = 0
 let guestCounter = 0
 
-function addBtn(rival, number){
-    if (rival == "Home"){
+function checkWinner() {
+
+    if (homeCounter > guestCounter) {
+        homeBorder.style.borderColor = '#27F549'
+        guestBorder.style.borderColor = ''
+    }
+    if (homeCounter < guestCounter) {
+        guestBorder.style.borderColor = '#27F549'
+        homeBorder.style.borderColor = ''
+    }
+    if (homeCounter == guestCounter) {
+        guestBorder.style.borderColor = '#2750F5'
+        homeBorder.style.borderColor = '#2750F5'
+    }
+
+}
+
+function clearBorder() {
+    guestBorder.style.borderColor = ''
+    homeBorder.style.borderColor = ''
+}
+
+function addBtn(rival, number) {
+    if (rival == "Home") {
         homeCounter += number;
         homeScore.innerText = homeCounter
-    }else{
-        
+    } else {
+        guestCounter += number;
+        guestScore.innerText = guestCounter;
     }
+    checkWinner()
 }
+
+function clearCounters() {
+    homeCounter = 0
+    guestCounter = 0
+    homeScore.innerText = homeCounter
+    guestScore.innerText = guestCounter
+    clearBorder()
+}
+
+clearButton.addEventListener("click", () => clearCounters())
+
+
 
 plus1Home.addEventListener("click", () => addBtn("Home", 1));
 plus2Home.addEventListener("click", () => addBtn("Home", 2));
 plus3Home.addEventListener("click", () => addBtn("Home", 3));
 
+
+plus1Guest.addEventListener("click", () => addBtn("Guest", 1))
+plus2Guest.addEventListener("click", () => addBtn("Guest", 2))
+plus3Guest.addEventListener("click", () => addBtn("Guest", 3))
+
+
+saveButton.addEventListener("click", () => saveResult())
+
+function saveResult() {
+    const newMatch = document.createElement("p")
+    newMatch.textContent = " Home " + homeCounter + " - " + guestCounter + " Guest "
+    document.getElementById("results-container").append(newMatch)
+    clearCounters()
+}
